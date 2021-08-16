e223a6530be8e6fafafa0e2583014a53eb5602bd
Mark Kriaf <22143612+mkriaf@users.noreply.github.com>
2021-08-16 10:51:51 -0700

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/testpost', methods = ['POST'])
+def postTesting():
+    name = request.form['name']
+    print (name)
+    return render_template('out.html')
