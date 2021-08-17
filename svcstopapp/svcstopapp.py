ab65e03120666b1956390b78ecaa2bc7517e2f00
Mark Kriaf <22143612+mkriaf@users.noreply.github.com>
2021-08-17 09:39:56 -0700

    updated htmls, and added subprocess

--- svcstopapp/svcstopapp.py
+++ svcstopapp/svcstopapp.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, request
+import os
+
+from werkzeug.utils import redirect
 application = Flask(__name__)
 
 @application.route('/')
 def index():
     return render_template('index.html')
 
+@application.route('/error')
+def error():
+    return render_template('error.html')
+
 @application.route('/testpost', methods = ['POST'])
 def postTesting():
     name = request.form['name']
-    print (name)
-    return render_template('out.html')
+    if name == "the bird":
+        os.system("sudo systemctl status nginx")
+        print (name)
+        return render_template('out.html')
+    else:
+        return redirect('/error')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
